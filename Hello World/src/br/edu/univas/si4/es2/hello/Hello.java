f8cf9e66f0fc2551c2664595b3aca2fc38de307d
Diego Alves Openheimer <diegoop.open@hotmail.com>
2017-08-08 20:55:57 -0300

    Hello Wolrd

--- Hello World/src/br/edu/univas/si4/es2/hello/Hello.java
+++ Hello World/src/br/edu/univas/si4/es2/hello/Hello.java
@@ -1,11 +1,38 @@
 package br.edu.univas.si4.es2.hello;
 
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+
+
+
 public class Hello {
 	
 	public static void main(String[] args){
 		
-	
+		JFrame frame = new JFrame();
+		JPanel painel = new JPanel();
 		
+		JButton button = new JButton();
+		
+		button.setText("Hello World");
+		
+		painel.setSize(150, 150);
+		
+		button.setSize(15, 15);
+		
+		frame.setSize(400, 400);
+		painel.add(button);
+		
+		frame.add(painel);
+		
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		
+		frame.setVisible(true);
 		
 		
 		
